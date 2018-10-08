fab16e7daff857a3670c27131c714127a7ca8522
Liz Steak <lizmaepark@gmail.com>
2018-10-08 17:41:19 -0600

    array and index working

--- server.js
+++ server.js
@@ -1,74 +1,34 @@
 // DEPENDENCIES
+// * Your mission is to complete the app
+// * The app will need routes for and show.ejs
+// * The app will need views for and show.ejs
+const port = 3000;
 const express = require('express');
 const app = express();
+const Missions = require('./models/marsMissions.js')
 
-// run `npm install` to install dependencies in package.json
+app.get('/missions', (req, res) => {
+  res.send(Missions);
+})
 
-// * Your mission is to complete the app
-// * The app will need routes for index and show
-// * The app will need views for index and show
-//
+// app.get('/missions/:id', (req, res) => {
+//   res.send(Missions[req.params.id])
+// })
+
+// INDEX Route - no working with ID
+app.get('/missions/index', (req, res) => {
+  res.render('index.ejs');
+});
 // * Make it so you can click on a mission’s name on the index page, and be taken to that mission’s show page
-// * Bonus: add images to the data and have them display
-// * Bonus: add static css to style the pages
-
-// NOTES:
-// ejs has not been installed
-// views folder has not been created
-// views/missions folder has not been created
-
-// PORT
-const port = 3000;
-
-// DATA - put into marsMissions.js file inside of a models folder, for module.exports
-// remember to require it in the server
-const marsMissions = [
-  {
-    name: "Curiosity",
-    launchDate: "26 Nov 2011",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Opportunity",
-    launchDate: "8 Jul 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Spirit",
-    launchDate: "10 Jun 2003",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Sojourner",
-    launchDate: "4 Dec 1996",
-    operator: "NASA",
-    missionType: "Rover",
-    img: ""
-  },
-  {
-    name: "Rosetta",
-    launchDate: "2 Mar 2004",
-    operator: "ESA",
-    missionType: "Gravity Assist",
-    img: ""
-  }
-];
-
-// INDEX Route
-// send data to 'missions/index.ejs' view
-// the view should display just the names of each mission
-// display the mission names as <li> in a <ul> with the class name "missions"
 
 // SHOW Route
 // send data to 'missions/show.ejs' view
+app.get('/missions/index/:curiosity', (req, res) => {
+  res.render('show.ejs', {
+    mission = Missions(get.params[0])
+  });
+});
 // the view should display all the data for a single mission
-
 
 
 // LISTENER
@@ -77,3 +37,6 @@ app.listen(port, function() {
 })
 
 module.exports = app;
+
+// * Bonus: add images to the data and have them display
+// * Bonus: add static css to style the pages
