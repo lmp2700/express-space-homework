05366469064b756c29b7c39c176298926e72b7b8
Liz Steak <lizmaepark@gmail.com>
2018-10-08 18:56:02 -0600

    completed mvp

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // DEPENDENCIES
 // * Your mission is to complete the app
-// * The app will need routes for and show.ejs
 // * The app will need views for and show.ejs
 const port = 3000;
 const express = require('express');
 const app = express();
-const Missions = require('./models/marsMissions.js')
+const Missions = require('./models/marsMissions')
 
 // app.get('/missions', (req, res) => {
 //   res.send(Missions);
@@ -21,22 +20,20 @@ app.get('/missions', (req, res) => {
     mission: Missions
   });
 });
-// * Make it so you can click on a mission’s name on the index page, and be taken to that mission’s show page
 
 // SHOW Route
-// send data to 'missions/show.ejs' view
 app.get('/missions/:show', (req, res) => {
-  res.render('show.ejs', {
-    mission: Missions[req.params[0]]
-  })
+  res.render('missions/show.ejs', {
+    mission: Missions[req.params.show]
+  });
 });
-// the view should display all the data for a single mission
-
 
 // LISTENER
 app.listen(port, function() {
   console.log('Missions to Mars running on port: ', port);
 })
+
+
 
 module.exports = app;
 
